a25c62efdb6652177280659b321c18b7d39903dc
Tri Hermawan <31398628+tri-hermawan@users.noreply.github.com>
2019-05-14 22:53:02 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "trihermawan.gamer@gmail.com";
+$password = "Qq123123";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "1";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "2";
+
+
+
